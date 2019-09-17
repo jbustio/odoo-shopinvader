41f1679c1a87a36aa04dc747da245cf3f839aab1
François Honoré <francois.honore@acsone.eu>
2019-09-17 16:29:25 +0200

    Continue

--- shopinvader/services/invoice.py
+++ shopinvader/services/invoice.py
@@ -15,7 +15,7 @@ from odoo.osv import expression
 
 
 class InvoiceService(Component):
-    _inherit = "base.shopinvader.service"
+    _inherit = "shopinvader.abstract.mail.service"
     _name = "shopinvader.invoice.service"
     _usage = "invoice"
     _expose_model = "account.invoice"
