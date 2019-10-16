56f793e027cf6b3efd9f901e7d59f849b7713a8e
François Honoré <francois.honore@acsone.eu>
2019-10-16 17:04:22 +0200

    [10.0][UPD] Changes due to shopinvader invoice payment

--- shopinvader_invoice/tests/test_invoice_service.py
+++ shopinvader_invoice/tests/test_invoice_service.py
@@ -1,112 +1,39 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import fields
-from odoo.addons.shopinvader.tests.common import CommonCase
+from .common import CommonInvoiceCase
 
 
-class TestInvoiceService(CommonCase):
-    """
-    Tests for
-    """
-
+class TestInvoiceServiceAnonymous(CommonInvoiceCase):
     def setUp(self, *args, **kwargs):
-        super(TestInvoiceService, self).setUp(*args, **kwargs)
-        self.invoice_obj = self.env["account.invoice"]
-        self.journal_obj = self.env["account.journal"]
-        self.register_payments_obj = self.env["account.register.payments"]
-        self.sale = self.env.ref("shopinvader.sale_order_2")
-        self.partner = self.env.ref("shopinvader.partner_1")
-        self.product = self.env.ref("product.product_product_4")
-        self.bank_journal_euro = self.journal_obj.create(
-            {"name": "Bank", "type": "bank", "code": "BNK67"}
+        super(TestInvoiceServiceAnonymous, self).setUp(*args, **kwargs)
+        self.partner = self.env.ref("base.res_partner_2").copy()
+
+    def test_get_invoice_anonymous(self):
+        """
+        Test the get on guest mode (using anonymous user).
+        It should not return any result, even if the anonymous user has some
+        invoices
+        :return:
+        """
+        # Check first without invoice related to the anonymous user
+        result = self.service_guest.dispatch("search")
+        data = result.get("data", [])
+        self.assertFalse(data)
+        # Then create a invoice related to the anonymous user
+        invoice = self._create_invoice(
+            partner=self.backend.anonymous_partner_id, validate=True
         )
-        self.payment_method_manual_in = self.env.ref(
-            "account.account_payment_method_manual_in"
+        self.assertEquals(
+            invoice.partner_id, self.backend.anonymous_partner_id
         )
-        self.precision = 2
-        with self.work_on_services(partner=self.partner) as work:
-            self.service = work.component(usage="invoice")
-        with self.work_on_services(
-            partner=self.backend.anonymous_partner_id
-        ) as work:
-            self.service_guest = work.component(usage="invoice")
+        result = self.service_guest.dispatch("search")
+        data = result.get("data", [])
+        self.assertFalse(data)
+        return
 
-    def _check_data_content(self, data, invoices):
-        """
-        Check data based on given invoices
-        :param data: list
-        :param invoices: account.invoice recordset
-        :return: bool
-        """
-        # To have them into correct order
-        invoices = invoices.search(self.service._get_base_search_domain())
-        self.assertEquals(len(data), len(invoices))
-        for current_data, invoice in zip(data, invoices):
-            state_label = self._get_selection_label(invoice, "state")
-            type_label = self._get_selection_label(invoice, "type")
-            self.assertEquals(current_data.get("invoice_id"), invoice.id)
-            self.assertEquals(current_data.get("number"), invoice.number)
-            self.assertEquals(
-                current_data.get("date_invoice"), invoice.date_invoice
-            )
-            self.assertEquals(current_data.get("state"), invoice.state)
-            self.assertEquals(current_data.get("type"), invoice.type)
-            self.assertEquals(current_data.get("state_label"), state_label)
-            self.assertEquals(current_data.get("type_label"), type_label)
-            self.assertEquals(
-                current_data.get("amount_total"), invoice.amount_total
-            )
-            self.assertEquals(
-                current_data.get("amount_tax"), invoice.amount_tax
-            )
-            self.assertEquals(
-                current_data.get("amount_untaxed"), invoice.amount_untaxed
-            )
-            self.assertEquals(current_data.get("amount_due"), invoice.residual)
-        return True
 
-    def _confirm_and_invoice_sale(self, sale, payment=True):
-        """
-        Confirm the given SO and create an invoice.
-        Can also make the payment if payment parameter is True
-        :param sale: sale.order recordset
-        :param payment: bool
-        :return: account.invoice
-        """
-        sale.action_confirm()
-        for line in sale.order_line:
-            line.write({"qty_delivered": line.product_uom_qty})
-        invoice_id = sale.action_invoice_create()
-        invoice = self.env["account.invoice"].browse(invoice_id)
-        invoice.action_invoice_open()
-        invoice.action_move_create()
-        if payment:
-            self._make_payment(invoice)
-        return invoice
-
-    def _make_payment(self, invoice, journal=False, amount=False):
-        """
-        Make payment for given invoice
-        :param invoice: account.invoice recordset
-        :param amount: float
-        :return: bool
-        """
-        ctx = {"active_model": invoice._name, "active_ids": invoice.ids}
-        wizard_obj = self.register_payments_obj.with_context(ctx)
-        register_payments = wizard_obj.create(
-            {
-                "payment_date": fields.Date.today(),
-                "journal_id": self.bank_journal_euro.id,
-                "payment_method_id": self.payment_method_manual_in.id,
-            }
-        )
-        if journal:
-            register_payments.write({"journal_id": journal.id})
-        if amount:
-            register_payments.write({"amount": amount})
-        register_payments.create_payment()
-
+class TestInvoiceService(CommonInvoiceCase):
     def test_get_invoice_logged(self):
         """
         Test the get on a logged user.
