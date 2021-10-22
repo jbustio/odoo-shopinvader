2d3152c7e19e43bc31fee42e2e9112c3f2a93eb6
François Honoré <francois.honore@acsone.eu>
2021-10-22 16:14:04 +0200

    [14.0][FWP] Forward port of #596: pre-commit + python 3 + small improvements

--- shopinvader/models/track_external_mixin.py
+++ shopinvader/models/track_external_mixin.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, models
@@ -13,14 +12,16 @@ class TrackExternalMixin(models.AbstractModel):
     def is_rest_request(self):
         return self.env.context.get("shopinvader_request", False)
 
-    @api.multi
-    def _write(self, vals):
+    def _fill_last_external_update_date(self, vals):
         if self.is_rest_request():
-            vals["last_external_update_date"] = fields.Datetime.now()
-        return super(TrackExternalMixin, self)._write(vals)
+            vals.update({"last_external_update_date": fields.Datetime.now()})
 
-    @api.model
-    def create(self, vals):
-        if self.is_rest_request():
-            vals["last_external_update_date"] = fields.Datetime.now()
-        return super(TrackExternalMixin, self).create(vals)
+    def _write(self, vals):
+        self._fill_last_external_update_date(vals)
+        return super()._write(vals)
+
+    @api.model_create_multi
+    def create(self, values_list):
+        for vals in values_list:
+            self._fill_last_external_update_date(vals)
+        return super().create(values_list)
