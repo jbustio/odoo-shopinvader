e4461935504ccafacd184f273b0457cdc02a0d60
Quentin Groulard <quentin.groulard@acsone.eu>
2023-08-02 11:58:58 +0200

    [ADD] shopinvader_product
    [FIX] shopinvader_product: Fix mapping

---
+++ shopinvader_product/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Product",
+    "summary": """Adds shopinvader product fields and schemas""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": [
+        "base_sparse_field",
+        "product",
+        "pydantic",
+        "extendable",
+        "extendable_fastapi",
+    ],
+    "data": [],
+    "demo": [],
+    "external_dependencies": {
+        "python": ["extendable_pydantic>=1.1.1", "pydantic>=2.0.0"]
+    },
+    "installable": True,
+}
