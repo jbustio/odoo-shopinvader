0a10f0845e0403e82acc918b49ef3b133f899058
Quentin Groulard <quentin.groulard@acsone.eu>
2023-12-01 10:18:35 +0100

    [ADD] shopinvader_api_signin_jwt

---
+++ shopinvader_api_signin_jwt/routers/signin.py
@@ -0,0 +1,50 @@
+# Copyright 2023 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Response, status
+
+from odoo import api, models
+
+from odoo.addons.base.models.res_partner import Partner
+from odoo.addons.fastapi.dependencies import odoo_env
+from odoo.addons.fastapi_auth_jwt.dependencies import (
+    Payload,
+    auth_jwt_authenticated_payload,
+    auth_jwt_optionally_authenticated_partner,
+)
+
+signin_router = APIRouter(tags=["signin"])
+
+
+@signin_router.post("/signin", status_code=200)
+def signin(
+    # TODO use fastai_auth_jwt.auth_jwt_authenticated_odoo_env when it is fixed
+    env: Annotated[api.Environment, Depends(odoo_env)],
+    partner: Annotated[Partner, Depends(auth_jwt_optionally_authenticated_partner)],
+    payload: Annotated[Payload, Depends(auth_jwt_authenticated_payload)],
+    response: Response,
+) -> None:
+    if not partner:
+        env[
+            "shopinvader_api_signin_jwt.signin_router.helper"
+        ]._create_partner_from_payload(payload)
+        response.status_code = status.HTTP_201_CREATED
+    # TODO: Manage anonymous cart
+    # see https://github.com/shopinvader/odoo-shopinvader/issues/1428
+
+
+class ShopinvaderApSigninJwtRouterHelper(models.AbstractModel):
+    _name = "shopinvader_api_signin_jwt.signin_router.helper"
+    _description = "ShopInvader API Signin Jwt Router Helper"
+
+    @api.model
+    def _get_partner_vals(self, payload: Payload):
+        return {"name": payload.get("name"), "email": payload.get("email")}
+
+    @api.model
+    def _create_partner_from_payload(self, payload: Payload):
+        return self.env["res.partner"].browse(
+            self.env["res.partner"].sudo().create(self._get_partner_vals(payload)).id
+        )
