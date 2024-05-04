7df5abe146d18a4afbcd18090e87c88f618eab13
Olivier Nibart <olivier@naya-tec.com>
2024-05-04 11:49:04 +0200

    [FIX] shopinvader_image: use sha1 to generate hash
    The Python builtin hash function as no identical results between
    instances.

--- shopinvader_image/models/shopinvader_image_mixin.py
+++ shopinvader_image/models/shopinvader_image_mixin.py
@@ -3,6 +3,7 @@
 # Copyright 2021 Camptocamp SA (http://www.camptocamp.com)
 # @author Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from hashlib import sha1
 
 from odoo import fields, models
 
@@ -53,7 +54,11 @@ class ShopinvaderImageMixin(models.AbstractModel):
         self.ensure_one()
         if not self[self._image_field]:
             return False
-        return str(hash(self._get_images_store_hash_tuple()))
+        # NOTE : from python 3.9 on we should use
+        # usedforsecurity=False with sha1
+        return sha1(
+            str(self._get_images_store_hash_tuple()).encode("utf-8")
+        ).hexdigest()
 
     def _get_images_store_hash_timestamp(self):
         """Get the timestamp of the last modification of the images
